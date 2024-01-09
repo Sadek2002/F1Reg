4a0d388b2de9b7719e4c704216b1f57f7e6054d8
Renask <renask.dev@gmail.com>
2024-01-09 01:13:51 +0100

    created the relationships and tables+migrations needed for a leaderboard

---
+++ app/Models/race.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class race extends Model
+{/**
+ * We define our relationships here.
+ * A race can have many Results, so we give it a belongsToMany relationship with the results table.
+ * See the comment in database->migration-> Create_race_result_table for more detail about the relationships.
+ */
+    use HasFactory;
+    public function results()
+    {
+        return $this->belongsToMany(Result::class);
+    }
+}
---
+++ database/factories/ResultFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Result>
+ */
+class ResultFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {/**
+     * In our return statement we reference our User factory to create a user and connect a lap time that's between 1 - 10.
+     * If we run ' php artisan tinker ' in our Console and run the following code ' result::factory()->count(1)->create()  '
+     * The factory will create a user for us, and give the user a fake lap time. this is very useful for testing.
+    */
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'laptime' => fake()->numberBetween(1,10),
+        ];
+    }
+}
